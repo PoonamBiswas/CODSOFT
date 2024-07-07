b2525effcaee9a00c1a46bdf0d164793622459d3
PoonamBiswas <90602111+PoonamBiswas@users.noreply.github.com>
2024-07-07 10:39:32 +0530

    Add files via upload
    "Guess the Number" is an engaging and interactive game where the player attempts to guess a randomly selected number within a specified range. The game offers multiple rounds, allowing players to improve their guessing skills and achieve a higher score. The player has a limited number of attempts (lives) to guess the correct number in each round, adding a layer of challenge and excitement.

---
+++ RandomNumber.java
@@ -0,0 +1,71 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomNumber {
+    private Scanner sc = new Scanner(System.in);
+    private int score = 0;
+    private int roundsWon = 0;
+
+    public void guessNumber(int min, int max, int rn) {
+        int lives = 5;
+        boolean guessedCorrectly = false;
+        System.out.println("YOU ONLY HAVE 5 LIVES");
+
+        while (lives > 0 && !guessedCorrectly) {
+            System.out.println("Guess the number: ");
+            int gn = sc.nextInt();
+
+            if (gn == rn) {
+                System.out.println("You guessed right!");
+                System.out.println("YOU WON WITH " + lives + " LIVES REMAINING");
+                score += lives; // Increase score based on remaining lives
+                roundsWon++;
+                guessedCorrectly = true;
+            } else if (gn < rn) {
+                System.out.println("Guess the number between " + gn + " and " + max + ".");
+                min = gn;
+            } else {
+                System.out.println("Guess the number between " + min + " and " + gn + ".");
+                max = gn;
+            }
+            lives--;
+
+            if (lives == 0 && !guessedCorrectly) {
+                System.out.println("You are out of lives. The correct number was " + rn + ".");
+            }
+        }
+    }
+
+    public void playGame() {
+        boolean playAgain = true;
+
+        while (playAgain) {
+            System.out.println("Enter the lower bound: ");
+            int min = sc.nextInt();
+            System.out.println("Enter the upper bound: ");
+            int max = sc.nextInt();
+
+            // Generate random number within the specified range
+            Random random = new Random();
+            int randomNumber = random.nextInt(max - min + 1) + min;
+
+            // Start the guessing game
+            guessNumber(min, max, randomNumber);
+
+            System.out.println("Do you want to play another round? (yes/no)");
+            String response = sc.next();
+
+            if (!response.equalsIgnoreCase("yes")) {
+                playAgain = false;
+            }
+        }
+
+        // Display final score
+        System.out.println("You played " + roundsWon + " rounds and your final score is: " + score);
+    }
+
+    public static void main(String[] args) {
+        RandomNumber randomNumberGame = new RandomNumber();
+        randomNumberGame.playGame();
+    }
+}
